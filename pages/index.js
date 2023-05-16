f2dfb3cfdcbb995cedcc3b351ec3ba21c26bae70
enicio <jessandro42@gmail.com>
2023-05-16 15:35:21 -0300

    Add id to NextJS anchor

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to <a id='next_link_title' href="https://nextjs.org">Next.js!</a>
         </h1>
 
         <div className={styles.description}>
